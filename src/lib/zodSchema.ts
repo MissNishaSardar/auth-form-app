be7a4658b8f4a1ecc3e8ea0a1c9671bb17754e38
Nisha Sardar <missnishasardar@gmail.com>
2025-11-19 19:42:14 +0530

    Register Schema created

---
+++ src/lib/zodSchema.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+export const RegisterSchema = z.object({
+	name: z.string().min(2, { error: " Input a valid user name" }),
+	email: z.email({ error: "Invalid Email" }),
+	password: z
+		.string()
+		.min(8, { error: "Password must be atleast 8 charecters" }),
+});
