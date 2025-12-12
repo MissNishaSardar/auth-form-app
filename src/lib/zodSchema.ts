d0b0c33653f33d6f25cd90c4b0cebe65b2d58a99
Nisha Sardar <missnishasardar@gmail.com>
2025-12-12 18:26:16 +0530

    Update zodSchema.ts

--- src/lib/zodSchema.ts
+++ src/lib/zodSchema.ts
@@ -1,9 +1,7 @@
 import z from "zod";
 
 export const registerSchema = z.object({
-	name: z.string().min(2, { error: " Input a valid user name" }),
-	email: z.email({ error: "Invalid Email" }),
-	password: z
-		.string()
-		.min(8, { error: "Password must be atleast 8 charecters" }),
+	name: z.string().min(2, " Input a valid user name"),
+	email: z.email("Invalid Email"),
+	password: z.string().min(8, "Password must be atleast 8 charecters"),
 });
