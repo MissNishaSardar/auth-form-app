34be17375430ccd6286f8603779ca641967183f6
Nisha Sardar <missnishasardar@gmail.com>
2025-12-12 18:49:38 +0530

    Update RegisterForm.tsx

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,12 +1,28 @@
 "use client";
 
+import { registerAtom } from "@/lib/atom";
 import { registerSchema } from "@/lib/zodSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useSetAtom } from "jotai";
+import { Loader, UserRoundPlus } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { Controller, useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 import z from "zod";
+import { Button } from "./shadcnui/button";
+import { Field, FieldError, FieldLabel } from "./shadcnui/field";
+import { Input } from "./shadcnui/input";
 
 const RegisterForm = () => {
-	const { handleSubmit, control } = useForm({
+	const setRegister = useSetAtom(registerAtom);
+	const router = useRouter();
+
+	const {
+		handleSubmit,
+		control,
+		formState: { isSubmitting },
+		reset,
+	} = useForm({
 		resolver: zodResolver(registerSchema),
 		defaultValues: {
 			name: "",
@@ -19,19 +35,90 @@ const RegisterForm = () => {
 	const handleRegister = async (register: z.infer<typeof registerSchema>) => {
 		await new Promise((resolve) => setTimeout(resolve, 1500));
 
-		// setRegister(register);
+		setRegister(register);
 
-		// toast.success("Registered successfully!");
+		toast.success("Registered successfully!");
 
-		// registerForm.reset();
+		reset();
 
-		// push("/");
+		router.push("/");
 	};
 
 	return (
 		<form
 			onSubmit={handleSubmit(handleRegister)}
-			className=""></form>
+			className="grid gap-6">
+			<Controller
+				name="name"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Field data-invalid={fieldState.invalid}>
+						<FieldLabel htmlFor={field.name}>Name</FieldLabel>
+						<Input
+							{...field}
+							id={field.name}
+							aria-invalid={fieldState.invalid}
+							placeholder="Enter your name"
+							autoComplete="given-name"
+						/>
+						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+					</Field>
+				)}
+			/>
+
+			<Controller
+				name="email"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Field data-invalid={fieldState.invalid}>
+						<FieldLabel htmlFor={field.name}>Email</FieldLabel>
+						<Input
+							{...field}
+							id={field.name}
+							aria-invalid={fieldState.invalid}
+							placeholder="Enter your email"
+							autoComplete="email"
+							type="email"
+						/>
+						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+					</Field>
+				)}
+			/>
+
+			<Controller
+				name="password"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Field data-invalid={fieldState.invalid}>
+						<FieldLabel htmlFor={field.name}>Password</FieldLabel>
+						<Input
+							{...field}
+							id={field.name}
+							aria-invalid={fieldState.invalid}
+							placeholder="Enter your password"
+							autoComplete="new-password"
+							type="password"
+						/>
+						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+					</Field>
+				)}
+			/>
+
+			<Button
+				type="submit"
+				className="cursor-pointer"
+				disabled={isSubmitting}>
+				{isSubmitting ? (
+					<>
+						<Loader className="animate-spin" /> Submitting
+					</>
+				) : (
+					<>
+						<UserRoundPlus /> Register
+					</>
+				)}
+			</Button>
+		</form>
 	);
 };
 
